e6c5c365ca232bb3f1bed7caa2bc0dc6e08f8635
mateusz <dalvan234@gmail.com>
2020-05-16 19:54:33 +0200

    fonts weight changed, navigation bar image hover changed, footer correct positioning, about image border changed

--- src/App.js
+++ src/App.js
@@ -1,40 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Route} from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import react from './react.png';
 
 import NavigationBar from './navigationBar/navigationBar.jsx';
 import HomeDisplay from './homeDisplay/homeDisplay.jsx';
 import About from './about/about.jsx';
 class App extends Component {
-  render(){
-    const HomePage = () =>{
-      return(
+
+  render() {
+    const HomePage = () => {
+      return (
         <>
-        <NavigationBar/>
-        <HomeDisplay/>
+          <HomeDisplay />
         </>
       );
     }
-    const AboutPage = () =>{
-      return(
+    const AboutPage = () => {
+      return (
         <>
-        <NavigationBar/>
-        <About/>
+          <About />
         </>
       );
     }
     return (
       <div className="App">
+        <NavigationBar />
         <Route exact path="/" component={HomePage} />
         <Route exact path="/About" component={AboutPage} />
         <div className="footer">
-                <h1>Made with </h1><img src={react} alt=""></img>
-            </div>
+          <div className="footer-content">
+          <h1>Made with </h1><img src={react} alt=""></img>
+          </div>
+          
+        </div>
       </div>
     );
   }
-  
+
 }
 
 export default App;
--- src/navigationBar/navigationBar.jsx
+++ src/navigationBar/navigationBar.jsx
@@ -1,23 +1,44 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './navigationBar.css';
 import {Link} from 'react-router-dom';
 import github from './github-logo.png';
 import linkedin from './linkedin-logo.png';
-import cv from './cv.png';
-function navigationBar() {
-    return (
-        <div className="navigation-bar">
-            <div className="right">
-            <Link to="/" style={{ textDecoration: 'none', color: "rgba(255, 255, 255, 0.7)" }}><h1>Home</h1></Link>
-                <Link to="/About" style={{ textDecoration: 'none', color: "rgba(255, 255, 255, 0.7)" }}><h1>About</h1></Link>
-                <Link to="/Projects" style={{ textDecoration: 'none', color: "rgba(255, 255, 255, 0.7)" }}><h1>Projects</h1></Link>
-                <Link to="/Contact" style={{ textDecoration: 'none', color: "rgba(255, 255, 255, 0.7)" }}><h1>Contact</h1></Link>
-                <a href="https://github.com/m-figon/"><img id="circle" src={github} alt=""/></a>
-                <a href="https://www.linkedin.com/in/mateusz-figo%C5%84/"><img  id="circle" src={linkedin} alt=""/></a>
-                <a href="https://drive.google.com/open?id=1IjOpfUKuKWuPkzt0AXClr1Ivmn4fe7as"><img  src={cv} alt=""/></a>
+import cv from './cv.svg';
+class navigationBar extends Component {
+    constructor(){
+        super();
+        this.state={
+            id1: "",
+            id2: "",
+            id3: "",
+            id4: "",
+        }
+    }
+    click(array){
+        this.setState({
+            id1: "",
+            id2: "",
+            id3: "",
+            id4: "",
+            [array]: "selected"
+        })
+    }
+    render(){
+        return (
+            <div className="navigation-bar">
+                <div className="right">
+                <Link to="/" style={{ textDecoration: 'none', color: "rgba(255, 255, 255, 0.7)" }}><h1 id={this.state.id1} onClick={()=>this.click('id1')}>Home</h1></Link>
+                    <Link to="/About" style={{ textDecoration: 'none', color: "rgba(255, 255, 255, 0.7)" }}><h1 id={this.state.id2} onClick={()=>this.click('id2')}>About</h1></Link>
+                    <Link to="/Projects" style={{ textDecoration: 'none', color: "rgba(255, 255, 255, 0.7)" }}><h1 id={this.state.id3} onClick={()=>this.click('id3')}>Projects</h1></Link>
+                    <Link to="/Contact" style={{ textDecoration: 'none', color: "rgba(255, 255, 255, 0.7)" }}><h1 id={this.state.id4} onClick={()=>this.click('id4')}>Contact</h1></Link>
+                    <a href="https://github.com/m-figon/"><img id="circle" src={github} alt=""/></a>
+                    <a href="https://www.linkedin.com/in/mateusz-figo%C5%84/"><img  id="circle" src={linkedin} alt=""/></a>
+                    <a href="https://drive.google.com/open?id=1IjOpfUKuKWuPkzt0AXClr1Ivmn4fe7as"><img  src={cv} alt=""/></a>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+    
 }
 
 export default navigationBar
